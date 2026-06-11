e51c3ac6355799f331b88d7bb9080772d5863ecb
ignaciomiranda3 <fifaignacio.gmiranda@hotmail.com>
2026-06-10 22:23:32 -0300

    app_back.py
    agregue el blueprint de cancelacion

--- backend/app_back.py
+++ backend/app_back.py
@@ -5,6 +5,7 @@ from rutas.admin_rutas import admin_bp
 from rutas.clientes_rutas import clientes_bp
 from rutas.profesionales_rutas import profesionales_bp
 from rutas.auth_rutas import auth_bp
+from rutas.cancelacion_rutas import cancelacion_bp  
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FRONTEND_TEMPLATES = os.path.abspath(os.path.join(BASE_DIR, "..", "frontend", "templates"))
@@ -16,6 +17,7 @@ app.register_blueprint(admin_bp, url_prefix='/admin')
 app.register_blueprint(clientes_bp, url_prefix='/clientes')
 app.register_blueprint(profesionales_bp, url_prefix='/profesionales')
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
+ app.register_blueprint(cancelacion_bp)
 
 @app.route('/')
 def index():
